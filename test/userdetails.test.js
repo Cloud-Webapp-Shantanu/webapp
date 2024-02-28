eddaff080e2933858bd56ca447fdff197e863453
tyagishantanu <tyagi.sha@northeastern.edu>
2024-02-28 12:14:30 -0500

    async added

--- test/userdetails.test.js
+++ test/userdetails.test.js
@@ -2,6 +2,11 @@
 const { app, server } = require("../app.js");
 const supertest = require("supertest");
 const request = supertest(app);
+const sequelize = require('../connection.js');
+
+beforeAll(async () => {
+    await sequelize.sync({ force: true });
+});
 
 afterAll((done) => {
     server.close(done);
