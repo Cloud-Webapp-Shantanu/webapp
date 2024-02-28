e068a852e36c0276a4df15a8bbffbe9654f0d1f1
tyagishantanu <tyagi.sha@northeastern.edu>
2024-02-28 15:24:28 -0500

    added else for non-localhost

--- connection.js
+++ connection.js
@@ -3,11 +3,25 @@ const { Sequelize } = require('sequelize');
 
 let sequelize;
 
-sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  port: process.env.DB_PORT,
-});
+if (process.env.DB_HOST === 'localhost') {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+  });
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+  });
+}
 
 sequelize.authenticate()
   .then(() => {
