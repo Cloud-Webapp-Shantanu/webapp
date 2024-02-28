c97c873c3c799d744ba7b92a1ea7aa2643cc8d2d
tyagishantanu <tyagi.sha@northeastern.edu>
2024-02-28 15:55:49 -0500

    changed path for .env

--- setup.sh
+++ setup.sh
@@ -43,6 +43,6 @@ echo "Creating user and changing directory ownership"
 echo "================================================================="
 sudo adduser csye6225 --shell /usr/sbin/nologin || { echo "Failed to add csye6225 user. Exiting."; exit 1; }
 sudo chown -R csye6225:csye6225 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
-sudo chmod -R 744 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
+sudo chmod -R 755 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
 
 echo "=======================ALL DONE==================================="
