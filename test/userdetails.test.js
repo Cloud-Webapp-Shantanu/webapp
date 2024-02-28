9c22e97542dcda81e401d3002da3bec530a6064f
tyagishantanu <tyagi.sha@northeastern.edu>
2024-02-28 12:08:56 -0500

    Removed unwanted code for testcase

--- test/userdetails.test.js
+++ test/userdetails.test.js
@@ -2,14 +2,6 @@
 const { app, server } = require("../app.js");
 const supertest = require("supertest");
 const request = supertest(app);
-const user = require('../model/User.js');
-const sequalize = require('../connection.js');
-
-// beforeAll(async () => {
-//     await sequalize.sync({ force: true });
-//     await user.sync();
-//     console.log('user table synced to database');
-// });
 
 afterAll((done) => {
     server.close(done);
