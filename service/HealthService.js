ca999974738948e62196eda80610bf909b920736
tyagishantanu <tyagi.sha@northeastern.edu>
2024-03-16 22:29:17 -0400

    added logger import

--- service/HealthService.js
+++ service/HealthService.js
@@ -1,11 +1,12 @@
 const sequelize = require('../connection.js');
+const { logger } = require('../winston-log/winston');
 
 async function checkDatabaseConnection() {
   try {
     await sequelize.authenticate();
     return true;
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    logger.error('Unable to connect to the database:', error);
     return false;
   }
 }
