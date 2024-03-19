db7cde9c7a6adcb4d08f0f3d601a652754ee09ee
tyagishantanu <tyagi.sha@northeastern.edu>
2024-03-19 12:49:25 -0400

    changed log format

--- winston-log/winston.js
+++ winston-log/winston.js
@@ -1,19 +1,29 @@
 const winston = require("winston");
 const { transports, format } = require("winston");
 
-const customFormat = format.printf(({ timestamp, level, message }) => {
-  return `(${timestamp}) - [${level.toUpperCase()}] - "${message}"`;
-});
+// const customFormat = format.printf(({ timestamp, level, message }) => {
+//   return `(${timestamp}) - [${level.toUpperCase()}] - "${message}"`;
+// });
+
+const useFormat = format.combine(
+  format((info, opts) => {
+    let level = info.level.toUpperCase();
+      if(level === 'VERBOSE') {
+        level = 'DEBUG';
+      }
+
+      info['severity'] = level;
+      delete info.level;
+      return info;
+  })(),
+  format.json());
 
 const logger = winston.createLogger({
-    level: "info",
+    level: 'info',
     format: format.combine(
         format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        customFormat
+        useFormat
     ),
-    // defaultMeta: {
-    //   service: "webapp",
-    // },
     transports: [
       // new winston.transports.Console(),
       // process.env.ENV === 'dev' ? new transports.File({ filename: "./logs/audit.log", level: "error" }) : new transports.File({ filename: "./var/log/audit.log", level: "error" }),
