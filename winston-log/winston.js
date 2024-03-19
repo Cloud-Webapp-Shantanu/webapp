6493b6381eed47f077aae9f5b0dfc9e6a0b28364
tyagishantanu <tyagi.sha@northeastern.edu>
2024-03-19 13:45:26 -0400

    Winston changes fixes

--- winston-log/winston.js
+++ winston-log/winston.js
@@ -5,30 +5,39 @@ const { transports, format } = require("winston");
 //   return `(${timestamp}) - [${level.toUpperCase()}] - "${message}"`;
 // });
 
-const useFormat = format.combine(
-  format((info, opts) => {
-    let level = info.level.toUpperCase();
-      if(level === 'VERBOSE') {
-        level = 'DEBUG';
-      }
+const useFormat = format.printf(({ timestamp, level, message }) => {
+  let severity = 'DEFAULT';
+  if (level === 'error' || level === 'critical') {
+    severity = 'ERROR';
+  } else if (level === 'warn') {
+    severity = 'WARNING';
+  } else if (level === 'info') {
+    severity = 'INFO';
+  } else if (level === 'verbose' || level === 'debug') {
+    severity = 'DEBUG';
+  }
 
-      info['severity'] = level;
-      delete info.level;
-      return info;
-  })(),
-  format.json());
+  // Prepare log entry
+  const logEntry = {
+    timestamp,
+    severity,
+    message,
+  };
+
+  return JSON.stringify(logEntry);
+});
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: format.combine(
-        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        useFormat
-    ),
-    transports: [
-      // new winston.transports.Console(),
-      // process.env.ENV === 'dev' ? new transports.File({ filename: "./logs/audit.log", level: "error" }) : new transports.File({ filename: "./var/log/audit.log", level: "error" }),
-      process.env.ENV === 'dev' ? new transports.File({ filename: "./logs/webapp.log" }) : new transports.File({ filename: "/var/log/webapp/webapp.log" }),
-    ],
+  level: 'info',
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    useFormat
+  ),
+  transports: [
+    // new winston.transports.Console(),
+    // process.env.ENV === 'dev' ? new transports.File({ filename: "./logs/audit.log", level: "error" }) : new transports.File({ filename: "./var/log/audit.log", level: "error" }),
+    process.env.ENV === 'dev' ? new transports.File({ filename: "./logs/webapp.log" }) : new transports.File({ filename: "/var/log/webapp/webapp.log" }),
+  ],
 });
 
 module.exports = { logger, winston };
