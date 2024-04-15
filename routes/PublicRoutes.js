ba90fb90bfdaa69f58efa0023be3504b0b73f2db
tyagishantanu <tyagi.sha@northeastern.edu>
2024-04-15 15:44:56 -0400

    test check for rolling update

--- routes/PublicRoutes.js
+++ routes/PublicRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const healthController = require('../controller/HealthController.js');
 const userController = require("../controller/UserController.js");
 
-router.post('/v1/user', userController.createUser);
+router.post('/v8/user', userController.createUser);
 router.get('/healthz', healthController.gethealthCheck);
 router.use('/', healthController.healthCheckMiddleware);
-router.post('/v1/user/*', (req, res) => {
+router.post('/v8/user/*', (req, res) => {
     res.status(404).send();
 });
 
