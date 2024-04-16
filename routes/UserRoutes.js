7e35e576ee0c9abe59193cf783258cc1468545f2
tyagishantanu <tyagi.sha@northeastern.edu>
2024-04-15 21:01:48 -0400

    CHanged back to original

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require("../controller/UserController.js");
 
-router.get('/v8/user/self', userController.getUserById);
-router.put('/v8/user/self', userController.updateUserById);
-router.get('/v8/user/account-verification', userController.verifyAccount);
+router.get('/v1/user/self', userController.getUserById);
+router.put('/v1/user/self', userController.updateUserById);
+router.get('/v1/user/account-verification', userController.verifyAccount);
 
 module.exports = router;
